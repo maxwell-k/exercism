3e8c0be30127ab3a151ec10305e51cefc4bcd0f9
Keith Maxwell <keith.maxwell@gmail.com>
2025-11-02 22:14:22 +0000

    Iteration 1

--- dart/isogram/lib/isogram.dart
+++ dart/isogram/lib/isogram.dart
@@ -1,3 +1,10 @@
 class Isogram {
-  // Put your code here
+  bool isIsogram(String word) {
+    final List<String> runes = word
+        .toLowerCase()
+        .split("")
+        .where((i) => !{' ', '-'}.contains(i))
+        .toList();
+    return runes.toSet().every((e) => runes.where((i) => i == e).length == 1);
+  }
 }
