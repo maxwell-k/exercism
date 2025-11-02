dccdffa481ef13726500a65b33a43d3b46582bae
Keith Maxwell <keith.maxwell@gmail.com>
2025-11-02 22:04:47 +0000

    Iteration 1

--- dart/acronym/lib/acronym.dart
+++ dart/acronym/lib/acronym.dart
@@ -1,3 +1,7 @@
 class Acronym {
-  // Put your code here
+  String abbreviate(String phrase) => phrase
+      .toUpperCase()
+      .split(RegExp(r"[_ -]+"))
+      .map((String i) => i[0])
+      .join("");
 }
