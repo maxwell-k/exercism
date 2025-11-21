882cdcb103626d9a2cf324a19402218dcc7fb3fc
Keith Maxwell <keith.maxwell@gmail.com>
2025-11-21 16:07:27 +0000

    Iteration 1

--- dart/bob/lib/bob.dart
+++ dart/bob/lib/bob.dart
@@ -1,3 +1,20 @@
 class Bob {
-  // Put your code here
+  bool _yell(String me) => me != me.toLowerCase() && me == me.toUpperCase();
+  bool _question(String me) => me.endsWith("?");
+
+  String response(String message) {
+    final me = message.trim();
+
+    if (_yell(me) && _question(me)) {
+      return "Calm down, I know what I'm doing!";
+    } else if (_yell(me)) {
+      return "Whoa, chill out!";
+    } else if (_question(me)) {
+      return "Sure.";
+    } else if (me.isEmpty) {
+      return "Fine. Be that way!";
+    }
+
+    return "Whatever.";
+  }
 }
