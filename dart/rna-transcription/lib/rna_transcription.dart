afc6e9ea309be5bb5de3c73a107c189658bf71d4
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-12 09:46:54 +0000

    Iteration 1

--- dart/rna-transcription/lib/rna_transcription.dart
+++ dart/rna-transcription/lib/rna_transcription.dart
@@ -1,3 +1,19 @@
 class RnaTranscription {
-  // Put your code here
+  String toRna(String text) => text
+      .split("")
+      .map((c) {
+        switch (c) {
+          case 'G':
+            return 'C';
+          case 'C':
+            return 'G';
+          case 'T':
+            return 'A';
+          case 'A':
+            return 'U';
+          default:
+            throw ArgumentError("Invalid input");
+        }
+      })
+      .join("");
 }
