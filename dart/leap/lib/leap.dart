e3290a140ae9f057e2944d787d8e0e6a70b59013
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-12 09:07:57 +0000

    Iteration 1

--- dart/leap/lib/leap.dart
+++ dart/leap/lib/leap.dart
@@ -1,3 +1,8 @@
 class Leap {
-  // Put your code here
+  bool leapYear(int year) {
+    if (year % 400 == 0) return true;
+    if (year % 100 == 0) return false;
+    if (year % 4 == 0) return true;
+    return false;
+  }
 }
