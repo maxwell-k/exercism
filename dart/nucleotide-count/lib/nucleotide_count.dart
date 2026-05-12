156137ea7350b5caf2bd39828497e72af914ead2
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-12 10:51:33 +0000

    Iteration 2

--- dart/nucleotide-count/lib/nucleotide_count.dart
+++ dart/nucleotide-count/lib/nucleotide_count.dart
@@ -3,14 +3,14 @@ class InvalidNucleotideException implements Exception {}
 class NucleotideCount {
   Map<String, int> count(String strand) {
     Map<String, int> result = {"A": 0, "C": 0, "G": 0, "T": 0};
-    for (var i in strand.split("")) {
+    strand.split("").forEach((i) {
       int? before = result[i];
-      if (before == null) {
-        throw InvalidNucleotideException();
-      } else {
+      if (before != null) {
         result[i] = before + 1;
+      } else {
+        throw InvalidNucleotideException();
       }
-    }
+    });
     return result;
   }
 }
