1be6012d52dc8e2efd07453dfd3c5682e73dfc79
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-12 15:54:04 +0000

    Iteration 1

--- dart/scrabble-score/lib/scrabble_score.dart
+++ dart/scrabble-score/lib/scrabble_score.dart
@@ -1,1 +1,22 @@
-// Put your code here
+Map<String, int> table = const {
+  'A, E, I, O, U, L, N, R, S, T': 1,
+  'D, G': 2,
+  'B, C, M, P': 3,
+  'F, H, V, W, Y': 4,
+  'K': 5,
+  'J, X': 8,
+  'Q, Z': 10,
+};
+
+int score(String word) {
+  Map<String, int> scores = {};
+  table.forEach(
+    (letters, score) =>
+        letters.split(', ').forEach((letter) => scores[letter] = score),
+  );
+
+  return Iterable<int?>.generate(
+    word.length,
+    (i) => scores[word[i].toUpperCase()],
+  ).fold(0, (sum, i) => sum + (i ?? 0));
+}
