66548997d499b6d8486170739c9db1c6ebaefa5e
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-12 09:26:40 +0000

    Iteration 1

--- dart/raindrops/lib/raindrops.dart
+++ dart/raindrops/lib/raindrops.dart
@@ -1,3 +1,12 @@
+const words = {3: 'Pling', 5: 'Plang', 7: 'Plong'};
+
 class Raindrops {
-  // Put your code here
+  String convert(int number) {
+    String result = words.keys
+        .where((i) => number % i == 0)
+        .map((i) => words[i])
+        .join("");
+    if (result.isEmpty) result = number.toString();
+    return result;
+  }
 }
