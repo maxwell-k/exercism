ef49a3f8430acd3f503986c0e9edc7473e845609
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-22 11:26:11 +0000

    Iteration 1

--- dart/armstrong-numbers/lib/armstrong_numbers.dart
+++ dart/armstrong-numbers/lib/armstrong_numbers.dart
@@ -1,3 +1,9 @@
 class ArmstrongNumbers {
-  // Put your code here
+  bool isArmstrongNumber(String digits) {
+    BigInt sum = BigInt.from(0);
+    for (int i = 0; i < digits.length; i++) {
+      sum += BigInt.parse(digits[i]).pow(digits.length);
+    }
+    return BigInt.parse(digits) == sum;
+  }
 }
