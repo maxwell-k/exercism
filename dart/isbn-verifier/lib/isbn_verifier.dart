d1400f7824a79ebd15228fa646dfaeb18b253005
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-24 11:00:57 +0000

    Iteration 1

--- dart/isbn-verifier/lib/isbn_verifier.dart
+++ dart/isbn-verifier/lib/isbn_verifier.dart
@@ -1,1 +1,14 @@
-// Put your code here
+bool isValid(String isbn) {
+  List<String> characters = isbn.replaceAll("-", "").split("");
+  if (characters.length != 10) return false;
+
+  if (characters[9] == 'X') characters[9] = '10';
+  final List<int?> digits = characters.map(int.tryParse).toList();
+  if (digits.indexOf(null) >= 0) return false;
+
+  return Iterable<int>.generate(
+            10,
+          ).fold(0, (sum, i) => sum + digits[i]! * (10 - i)) %
+          11 ==
+      0;
+}
