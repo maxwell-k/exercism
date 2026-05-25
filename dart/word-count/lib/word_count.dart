94f3d165b79f13fb234858ec130b13547f99a1b5
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-25 10:29:11 +0000

    Iteration 1

--- dart/word-count/lib/word_count.dart
+++ dart/word-count/lib/word_count.dart
@@ -1,3 +1,16 @@
 class WordCount {
-  // Put your code here
+  Map<String, int> _add(Map<String, int> result, String key) {
+    result.update(key, (int value) => value + 1, ifAbsent: () => 1);
+    return result;
+  }
+
+  Map<String, int> countWords(String text) {
+    return text
+        .toLowerCase()
+        .split(RegExp(r'[ ,\n!!&@$%^:.]+'))
+        .map((i) => i.startsWith("'") ? i.substring(1) : i)
+        .map((i) => i.endsWith("'") ? i.substring(0, i.length - 1) : i)
+        .where((i) => i.isNotEmpty)
+        .fold(Map<String, int>(), _add);
+  }
 }
