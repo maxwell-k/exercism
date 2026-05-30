5aff3f53d0230ab30ee222abe7be5e97db0200c3
Keith Maxwell <keith.maxwell@gmail.com>
2026-05-30 16:32:28 +0000

    Iteration 1

--- dart/hamming/lib/hamming.dart
+++ dart/hamming/lib/hamming.dart
@@ -1,3 +1,10 @@
 class Hamming {
-  // Put your code here
+  int distance(String left, String right) {
+    if (left.length != right.length)
+      throw ArgumentError("strands must be of equal length");
+    return List<int>.generate(
+      left.length,
+      (i) => left[i] != right[i] ? 1 : 0,
+    ).fold(0, (total, current) => total + current);
+  }
 }
