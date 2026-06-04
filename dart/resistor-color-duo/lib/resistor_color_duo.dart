649a7d847480b5c299548494c4db064d9915cf36
Keith Maxwell <keith.maxwell@gmail.com>
2026-06-04 14:15:00 +0000

    Iteration 1

--- dart/resistor-color-duo/lib/resistor_color_duo.dart
+++ dart/resistor-color-duo/lib/resistor_color_duo.dart
@@ -1,3 +1,22 @@
+import 'dart:math' show pow;
+
+const colours = [
+  'black',
+  'brown',
+  'red',
+  'orange',
+  'yellow',
+  'green',
+  'blue',
+  'violet',
+  'grey',
+  'white',
+];
+
 class ResistorColorDuo {
-  // Put your code here
+  static const int length = 2;
+  int value(List<String> input) => List.generate(
+    length,
+    (int i) => colours.indexOf(input[i]) * pow(10, length - 1 - i) as int,
+  ).fold(0, (previous, element) => previous + element);
 }
