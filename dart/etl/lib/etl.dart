1f2bf456111f25941706248d50dbba1a54c53055
Keith Maxwell <keith.maxwell@gmail.com>
2026-06-05 07:24:22 +0000

    Iteration 2

--- dart/etl/lib/etl.dart
+++ dart/etl/lib/etl.dart
@@ -1,11 +1,7 @@
 class Etl {
-  Map<String, int> transform(Map<String, List<String>> legacy) {
-    Map<String, int> scores = {};
-    legacy.forEach(
-      (score, letters) => letters.forEach(
-        (letter) => scores[letter.toLowerCase()] = int.parse(score),
-      ),
-    );
-    return scores;
-  }
+  Map<String, int> transform(Map<String, List<String>> legacy) => {
+    for (MapEntry entry in legacy.entries)
+      for (String letter in entry.value)
+        letter.toLowerCase(): int.parse(entry.key),
+  };
 }
