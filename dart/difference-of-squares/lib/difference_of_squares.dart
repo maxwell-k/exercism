4631d036df81ea0c887b069925d03f86ca1f11e9
Keith Maxwell <keith.maxwell@gmail.com>
2026-07-07 21:41:44 +0000

    Iteration 1

--- dart/difference-of-squares/lib/difference_of_squares.dart
+++ dart/difference-of-squares/lib/difference_of_squares.dart
@@ -1,3 +1,19 @@
+import 'dart:math' show pow;
+
 class DifferenceOfSquares {
-  // Put your code here
+  num squareOfSum(num n) {
+    final num sum = Iterable<int>.generate(
+      n as int,
+      (int i) => i + 1,
+    ).fold(0, (prev, element) => prev + element);
+    return pow(sum, 2);
+  }
+
+  num sumOfSquares(num n) {
+    if (n == 1) return 1; // avoid rounding error
+    // https://en.wikipedia.org/wiki/Square_pyramidal_number
+    return (pow(n, 3) / 3) + (pow(n, 2) / 2) + (n / 6);
+  }
+
+  num differenceOfSquares(num n) => squareOfSum(n) - sumOfSquares(n);
 }
