5442a34fb1bd6ab894c6bf95706f0d293b495fc7
Keith Maxwell <keith.maxwell@gmail.com>
2026-07-10 12:57:44 +0000

    Iteration 3

--- dart/collatz-conjecture/lib/collatz_conjecture.dart
+++ dart/collatz-conjecture/lib/collatz_conjecture.dart
@@ -1,14 +1,10 @@
 class CollatzConjecture {
   int steps(int n) {
     if (n < 1) throw ArgumentError('Only positive integers are allowed');
-    int count = 0;
-    while (n != 1) {
-      if (n % 2 == 0)
-        n ~/= 2;
-      else
-        n = 3 * n + 1;
-      count++;
+    int result;
+    for (result = 0; n != 1; result++) {
+      n = n % 2 == 0 ? n ~/ 2 : 3 * n + 1;
     }
-    return count;
+    return result;
   }
 }
