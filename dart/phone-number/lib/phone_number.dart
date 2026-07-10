ad0d79622fceff4a60099d03f7137a3a79ff0a2e
Keith Maxwell <keith.maxwell@gmail.com>
2026-07-10 15:46:05 +0000

    Iteration 2

--- dart/phone-number/lib/phone_number.dart
+++ dart/phone-number/lib/phone_number.dart
@@ -1,28 +1,25 @@
 class PhoneNumber {
   String? clean(String input) {
-    String result = input.replaceAll(RegExp(r'[() -.]+'), '');
-    if (RegExp(r'[@:!]').hasMatch(result))
+    final cleaned = input.replaceAll(RegExp(r'[() -.]+'), '');
+    if (RegExp(r'[@:!]').hasMatch(cleaned))
       throw FormatException('punctuations not permitted');
-    if (RegExp(r'[a-zA-Z]').hasMatch(result))
+    if (RegExp(r'[a-zA-Z]').hasMatch(cleaned))
       throw FormatException('letters not permitted');
-    if (result.length > 11)
+    if (cleaned.length > 11)
       throw FormatException('must not be greater than 11 digits');
-    if (result.length == 11) {
-      if (result.startsWith("1")) {
-        result = result.substring(1);
-      } else {
-        throw FormatException('11 digits must start with 1');
-      }
-    }
-    if (result.length < 10)
+    if (cleaned.length == 11 && cleaned[0] != "1")
+      throw FormatException('11 digits must start with 1');
+    if (cleaned.length < 10)
       throw FormatException('must not be fewer than 10 digits');
-    if (result.startsWith("0"))
+
+    final result = cleaned.length == 10 ? cleaned : cleaned.substring(1);
+    if (result[0] == "0")
       throw FormatException('area code cannot start with zero');
-    if (result.startsWith("1"))
+    if (result[0] == "1")
       throw FormatException('area code cannot start with one');
-    if (result.substring(3).startsWith("0"))
+    if (result[3] == "0")
       throw FormatException('exchange code cannot start with zero');
-    if (result.substring(3).startsWith("1"))
+    if (result[3] == "1")
       throw FormatException('exchange code cannot start with one');
     return result;
   }
