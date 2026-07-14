78d354ec204ce9d8d75dd899aebb5997b6915811
Keith Maxwell <keith.maxwell@gmail.com>
2026-07-14 17:38:41 +0000

    Iteration 1

--- dart/binary-search-tree/lib/binary_search_tree.dart
+++ dart/binary-search-tree/lib/binary_search_tree.dart
@@ -1,3 +1,32 @@
+class Node<T extends Comparable<T>> {
+  final T data;
+  Node<T>? left, right;
+
+  Node(this.data);
+
+  void insert(T value) {
+    bool _left = value.compareTo(data) <= 0;
+    if (_left && left == null)
+      left = Node<T>(value);
+    else if (!_left && right == null)
+      right = Node<T>(value);
+    else
+      (_left ? left : right)!.insert(value);
+  }
+
+  Iterable<T> get sortedData sync* {
+    for (var t in left?.sortedData ?? <T>[]) yield t;
+    yield data;
+    for (var t in right?.sortedData ?? <T>[]) yield t;
+  }
+}
+
 class BinarySearchTree {
-  // Put your code here
+  Node<String> root;
+
+  BinarySearchTree(String data) : root = Node<String>(data);
+
+  void insert(String data) => root.insert(data);
+
+  Iterable<Comparable> get sortedData => this.root.sortedData;
 }
